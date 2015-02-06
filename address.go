590dcdc7b0c64e18ceb18c6fa0d5b207db7ba925
mably <f.mably@gmail.com>
2015-02-06 10:37:39 +0100

    Update btcnet path import paths to new location.

--- address.go
+++ address.go
@@ -8,9 +8,9 @@ import (
 	"encoding/hex"
 	"errors"
 
-	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcec"
 	"github.com/ppcsuite/btcutil/base58"
+	"github.com/ppcsuite/ppcd/chaincfg"
 	"golang.org/x/crypto/ripemd160"
 )
 
